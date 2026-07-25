efd3daecdaa0a7a8323a56a4e604e57f40141689
Morochief <moroasdfg@gmail.com>
2026-07-25 08:22:29 -0300

    chore: Update Supabase keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // En Astro, las variables públicas del lado del cliente comienzan con PUBLIC_
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL || 'https://lfhxwamctujvgszmdjap.supabase.co';
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmaHh3YW1jdHVqdmdzem1kamFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1ODIzMzEsImV4cCI6MjEwMDE1ODMzMX0.UShXQhDKW4RQwN2bZ_RDbn59hkDY0YWR6BDcNLZm3SQ';
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_FoUBe2a_2bsqfH_Smw0Waw_sopBzlNK';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
